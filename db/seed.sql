083c3240e72c232d95effd7ecf3469d2ab3da5b9
Star-Gibson <sgibsoncoding@gmail.com>
2020-12-12 15:02:07 -0500

    Created database, inserted seeds

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+
+-- USE APPROPRIATE DB
+USE burgers_db;
+
+--INSERTS VALUES INTO BURGERS TABLE
+INSERT INTO burgers (burger_name, devoured) VALUES ("BBQ BACON",false), ("JACK BACON SUPREME", false), ("ANIMAL STYLE", false);
