62cae6cb41d63ad8389f5d0e2ef330a6b9d9f957
Star-Gibson <sgibsoncoding@gmail.com>
2020-12-12 16:29:47 -0500

    updated burger_controller.js

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,50 @@
+//DEPENDENCIES
+var express = require("express");
+
+var router = express.Router();
+
+//IMPORT BURGER.JS MODEL
+var burger = require("../models/burger")
+
+//GET ROUTE
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+//POST ROUTE
+router.post("/api/burgers", function (req, res) {
+    burger.insertOne([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ],
+        function (result) {
+            //SEND BACK THE ID OF THE NEW BURGER
+            res.json({ id: result.insertId });
+        });
+});
+
+//PUT ROUTE
+router.put("/api/burgers/:id", function (req, res) {
+    let condition = "id = " + req.params.id;
+    console.log("condition", condition);
+
+    burger.updateOne(
+        { devoured: req.body.devoured }, condition, function (result) {
+            if (result.changedRows == 0) {
+                return res.sendStatus(404);
+            }
+            else {
+                res.sendStatus(200);
+            }
+        }
+    )
+});
+
+module.exports = router;
